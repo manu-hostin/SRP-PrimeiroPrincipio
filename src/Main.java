5d71caee87a14d28f514b01e0404cc38f99d99e3
Emanuelle Cristina Hostin <emanuelle_hostin@estudante.sesisenai.org.br>
2026-02-05 16:53:08 -0300

    Feat: Arruma pedido na Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Pedido pedido = new Pedido("123", 450.00, 2, "cliente@example.com");
+        Pedido pedido = new Pedido("PED-123", 600.00, 5, "cliente@email.com");
 
         ServicoPedido servicoDePedido = new ServicoPedido();
         servicoDePedido.processarPedido(pedido);
